847948f31641e637e70568d4d601c7c6562c71c2
Christian Cesar <christiancnp@gmail.com>
2021-03-28 20:41:38 -0400

    includes style global, unform components, component isolation, show errors, validation values

--- src/pages/SignUp/styles.ts
+++ src/pages/SignUp/styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import { shade } from 'polished';
-import signInBackgroundImg from '../../assets/sign-in-background.png';
+import signUpBackgroundImg from '../../assets/sign-up-background.png';
 
 export const Container = styled.div`
   height: 100vh;
@@ -38,7 +38,7 @@ export const Content = styled.div`
   }
   > a {
     margin-top: 24px;
-    color: #ff9000;
+    color: #f4ede8;
     display: block;
     text-decoration: none;
     transition: color 0.2s;
@@ -50,13 +50,13 @@ export const Content = styled.div`
       margin-right: 16px;
     }
     &:hover {
-      color: ${shade(0.2, '#ff9000')};
+      color: ${shade(0.2, '#f4ede8')};
     }
   }
 `;
 
 export const Background = styled.div`
   flex: 1;
-  background: url(${signInBackgroundImg}) no-repeat center;
+  background: url(${signUpBackgroundImg}) no-repeat center;
   background-size: cover;
 `;
